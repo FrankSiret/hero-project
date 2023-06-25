738cb1b9a6fa37d92a677e45c899b26efad3832f
FrankSiret <frank.siret@gmail.com>
2023-06-25 08:54:00 -0400

    initial commit -just after spring initializr

---
+++ src/main/java/com/test/hero/heroproject/HeroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.hero.heroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeroProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/hero/heroproject/HeroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.hero.heroproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
