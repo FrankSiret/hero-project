6de21781cf18ca331181e74f607f68ae8b79c51f
FrankSiret <frank.siret@gmail.com>
2023-06-26 13:53:42 -0400

    clean project

--- src/main/java/com/test/hero/heroproject/HeroProjectApplication.java
+++ src/main/java/com/test/hero/heroproject/HeroProjectApplication.java
@@ -1,40 +1,11 @@
 package com.test.hero.heroproject;
 
-import com.test.hero.heroproject.domain.Authority;
-import com.test.hero.heroproject.domain.User;
-import com.test.hero.heroproject.repository.AuthorityRepository;
-import com.test.hero.heroproject.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.Set;
 
 @SpringBootApplication
 public class HeroProjectApplication {
-
-    private final PasswordEncoder passwordEncoder;
-
-    private final UserRepository userRepository;
-    private final AuthorityRepository authorityRepository;
-
-    HeroProjectApplication(PasswordEncoder passwordEncoder, UserRepository userRepository, AuthorityRepository authorityRepository) {
-        this.passwordEncoder = passwordEncoder;
-        this.userRepository = userRepository;
-        this.authorityRepository = authorityRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(HeroProjectApplication.class, args);
     }
-
-    @Bean
-    public CommandLineRunner run() {
-        return args -> {
-
-        };
-    }
-
 }
